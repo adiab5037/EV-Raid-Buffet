fa1acb3264c080e7bfb80e6a02fcb176035e6b34
adiab5037 <adiab5037@gmail.com>
2026-09-16 15:38:56 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zad-cache-v10';
+const CACHE = 'zad-cache-v11';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
